35d5c58a3719c01f111d9273b4f9d001c3ce5c8f
Emma Dann <ed6@sanger.ac.uk>
2022-09-22 15:24:17 +0000

    api structure

---
+++ src/oor_benchmark/api.py
@@ -0,0 +1,13 @@
+from anndata import AnnData
+
+
+def check_dataset(adata: AnnData):
+    """Check that dataset output fits expected API."""
+
+
+def check_method(adata: AnnData):
+    """Check that method output fits expected API."""
+
+
+def sample_dataset():
+    """Create a simple dataset to use for testing methods in this task."""
